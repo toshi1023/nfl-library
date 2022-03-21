426d26b70efcae374a354b7332262076e1e3faa2
toshi1023 <toshimura1992@gmail.com>
2022-03-21 22:22:56 +0900

    add players table migration file

---
+++ database/migrations/2022_03_21_131856_create_players_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePlayersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('players', function (Blueprint $table) {
+            $table->increments('id')->comment('ID');
+            $table->string('firstname')->comment('名前');
+            $table->string('lastname')->comment('苗字');
+            $table->dateTime('birthday')->comment('生年月日');
+            $table->string('image_file')->nullable()->comment('画像名');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('players');
+    }
+}
