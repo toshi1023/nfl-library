8ea851f00fc2e998237f2679f51a613965762ad1
tmurasaki <toshiya.murasaki@frontarc.co.jp>
2022-04-01 10:43:46 +0900

    csvインポート処理の修正

--- app/Console/Commands/CsvImport.php
+++ app/Console/Commands/CsvImport.php
@@ -32,19 +32,24 @@ class CsvImport extends Command
     }
 
     /**
-     * Execute the console command.
+     * madden nfl のcsvファイルから情報を取得し、DBに格納
      *
      * @return int
      */
     public function handle()
     {
-        $season = 2013;
+        $season = 2012;
         $teams = config('const.UrlTeams');
 
         $data = [];
+        $file = null;
         foreach($teams as $val) {
             // SplFileObjectの作成
-            $file = new \SplFileObject(storage_path('app/public/'.$season.'/'.$val.'_madden_nfl_'.config('const.Madden.'.strval($season)).'.csv'));
+            if($season > 2015) {
+                $file = new \SplFileObject(storage_path('app/public/'.$season.'/'.$val.'_madden_nfl_'.config('const.Madden.'.strval($season)).'_.csv'));
+            } else {
+                $file = new \SplFileObject(storage_path('app/public/'.$season.'/'.$val.'_madden_nfl_'.config('const.Madden.'.strval($season)).'.csv'));
+            }
 
             // 読み込み設定
             $file->setFlags(
@@ -54,26 +59,86 @@ class CsvImport extends Command
                 \SplFileObject::DROP_NEW_LINE   // 改行コードは無視する
             );
             // 1行ずつ読み込んで配列に保存
+            $i = 0;
+            // 配列のindexを設定
+            $teamIndex = null;
+            $fnameIndex = null;
+            $lnameIndex = null;
+            $nameIndex = null;
+            $posIndex = null;
+            $numIndex = null;
+            $ratIndex = null;
             foreach($file as $member){
-                // $members[] = $member;
-                // dump($member[0]);
-                // dump($member[1]);
-                // dump($member[2]);
-                // dump($member[3]);
-                // dump($member[4]);
-                // dump($member[5]);
-                $data[$val]['team'] = $member[0];
-                $data[$val]['name'][] = $member[1].' '.$member[2];
-                $data[$val]['position'][] = $member[3];
-                $data[$val]['number'][] = $member[4];
-                $data[$val]['rating'][] = $member[5];
+                $j = 0;
+                foreach($member as $col) {
+                    // 条件設定
+                    if($i === 0) {
+                        switch($col) {
+                            case 'Team':
+                                $teamIndex = $j;
+                                break;
+                            case 'First Name':
+                                $fnameIndex = $j;
+                                break;
+                            case 'Last Name':
+                                $lnameIndex = $j; 
+                                break;
+                            case 'Position':
+                                $posIndex = $j;
+                                break;
+                            case 'Jersey':
+                            case 'Jersey Number':
+                                $numIndex = $j;
+                                break;
+                            case 'Overall':
+                            case 'OVR':
+                                $ratIndex = $j;
+                                break;
+                            default:
+                                // csvのデータカラムに"・Name"が存在するため、ここでチェック
+                                preg_match('/\bName\b/', $col, $matches);
+                                if($matches) $nameIndex = $j;
+
+                                // csvのデータカラムに"・Team"が存在するため、ここでチェック
+                                preg_match('/\bTeam\b/', $col, $matches);
+                                if($matches) $teamIndex = $j;
+
+                                break;
+                        }
+                        $j += 1;
+                    }
+                }
+                if(!is_null($teamIndex)) $data[$val]['team'] = $member[$teamIndex];
+                if(!is_null($fnameIndex) && !is_null($lnameIndex) && is_null($nameIndex)) {
+                    $data[$val]['name'][] = $member[$fnameIndex].' '.$member[$lnameIndex];
+                } else {
+                    $data[$val]['name'][] = $member[$nameIndex];
+                }
+                $data[$val]['position'][] = $member[$posIndex];
+                if(!is_null($numIndex)) $data[$val]['number'][] = $member[$numIndex];
+                $data[$val]['rating'][] = $member[$ratIndex];
+
+                $i += 1;
             }
         }
 
         foreach($teams as $team) {
             for($i = 0; $i < count($data[$team]['name']); $i++) {
-                // 例)【49ers】 No.7 QB : Colin Kaepernick - 89
-                dump(' 【'.$data[$team]['team'].'】 No.'.$data[$team]['number'][$i].' '.$data[$team]['position'][$i].' : '.$data[$team]['name'][$i].' - '.$data[$team]['rating'][$i]);
+                $is_team = array_key_exists('team', $data[$team]);
+                $is_num = array_key_exists('number', $data[$team]);
+                if($is_team && $is_num) {
+                    // 例)【49ers】 No.7 QB : Colin Kaepernick - 89
+                    dump(' 【'.$data[$team]['team'].'】 No.'.$data[$team]['number'][$i].' '.$data[$team]['position'][$i].' : '.$data[$team]['name'][$i].' - '.$data[$team]['rating'][$i]);
+                } else if ($is_team && !$is_num) {
+                    // 例)【49ers】 QB : Colin Kaepernick - 89
+                    dump(' 【'.$data[$team]['team'].'】 '.$data[$team]['position'][$i].' : '.$data[$team]['name'][$i].' - '.$data[$team]['rating'][$i]);
+                } else if (!$is_team && $is_num) {
+                    // 例)No.7 QB : Colin Kaepernick - 89
+                    dump('No.'.$data[$team]['number'][$i].' '.$data[$team]['position'][$i].' : '.$data[$team]['name'][$i].' - '.$data[$team]['rating'][$i]);
+                } else {
+                    // 例)QB : Colin Kaepernick - 89
+                    dump($data[$team]['position'][$i].' : '.$data[$team]['name'][$i].' - '.$data[$team]['rating'][$i]);
+                }
             }
         }
     }
