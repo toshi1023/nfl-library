fc8aa9c6b82108d4b426f7b35e93c6cfafa77d67
toshi1023 <>
2023-08-15 00:36:39 +0900

    誕生日の年数取得アクセサを実装

--- app/Models/Player.php
+++ app/Models/Player.php
@@ -24,8 +24,20 @@ class Player extends Model
     /**
      * アクセサ許可リスト
      */
-    protected $appends = ['birthday_date', 'image_url'];
+    protected $appends = ['birthday_year', 'birthday_date', 'image_url'];
     
+    /**
+     * 生年月日の年を取得(年齢算出に使用)
+     */
+    public function getBirthdayYearAttribute()
+    {
+        // 生年月日を設定(yyyy年MM月DD日)
+        if($this->birthday) {
+            return mb_substr($this->birthday, 0, 4);
+        }
+        return null;
+    }
+
     /**
      * 生年月日を取得
      */
@@ -35,7 +47,7 @@ class Player extends Model
         if($this->birthday) {
             return mb_substr($this->birthday, 0, 4).'年'.mb_substr($this->birthday, 4, 2).'月'.mb_substr($this->birthday, 6, 2).'日';
         }
-        return false;
+        return null;
     }
 
     /**
